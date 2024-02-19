4641e57abdff773532b372debe56a8ecd59868b3
eunhatbe <101296387+eunhatbe@users.noreply.github.com>
2024-02-19 11:32:14 +0900

    Create discord.py

---
+++ NEWS_BIRD/discord.py
@@ -0,0 +1,40 @@
+import asyncio
+
+import discord
+from discord.ext import commands
+
+TOKEN = "MTIwODg1NjcyOTEyMzgxOTYyMA.GW0AeT.rmHIUPd756t3hBGDiOVUjDu9FYElAqFwTVc5EA"
+
+
+class NewsBot:
+    def __init__(self):
+        pass
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"),
+                   intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f'Bot is ready')
+
+    channel = bot.get_channel(763897112404295703)
+    if channel is None:
+        print('채널이 없어요 🦉')
+        return
+    await channel.send("🦉안녕하세요 뉴스를 물어오는 뻐꾸기입니다🦉")
+
+
+@bot.command(name="run")
+async def test(ctx):
+    await ctx.send("run.")
+    print("test")
+
+async def main():
+    async with bot:
+        await bot.start(TOKEN)
+
+asyncio.run(main())
